f5fe4c440debbf33a3af0dd0a38b42891503d6e7
Owen McGirr <o.a.mcgirr@gmail.com>
2026-06-29 14:18:03 +0100

    Wire C# app startup lifecycle

--- src-dotnet/SwitchifyPc.App/App.xaml.cs
+++ src-dotnet/SwitchifyPc.App/App.xaml.cs
@@ -1,5 +1,46 @@
+using System.Windows;
+using SwitchifyPc.Core.AppLifecycle;
+using SwitchifyPc.Windows.AppLifecycle;
+
 namespace SwitchifyPc.App;
 
 public partial class App : System.Windows.Application
 {
+    private SingleInstanceService? singleInstance;
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+
+        ShutdownMode = ShutdownMode.OnExplicitShutdown;
+        singleInstance = new SingleInstanceService(new WindowsSingleInstanceLockFactory());
+        SingleInstanceDecision decision = singleInstance.Start(AppLaunchOptions.Parse(e.Args));
+
+        if (!decision.IsPrimaryInstance)
+        {
+            // TODO: signal the primary instance to show its main window for normal second launches.
+            Shutdown();
+            return;
+        }
+
+        if (decision.ShowMainWindow)
+        {
+            ShowMainWindow();
+        }
+    }
+
+    protected override void OnExit(ExitEventArgs e)
+    {
+        singleInstance?.Stop();
+        singleInstance = null;
+        base.OnExit(e);
+    }
+
+    private void ShowMainWindow()
+    {
+        SwitchifyPc.App.MainWindow window = new();
+        window.Closed += (_, _) => Shutdown();
+        MainWindow = window;
+        window.Show();
+    }
 }
